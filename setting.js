0205fd6480168b80eb892b1c50866d3626c2e02f
lollolol611 <leogaed@fds-waren.de>
2025-10-25 10:02:03 +0200

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,8 +1,8 @@
 //=====💫 sᴛᴀʀ xᴅ🔥====
 
 module.exports = {
-  BOT_NAME: 'sᴛᴀʀ xᴅ', // ur bot name
-  OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
+  BOT_NAME: 'star xd', // ur bot name
+  OWNER_NAME: '', //your name
   OWNER_NUMBER: '263776388689', // add your phone number
   SESSION_ID: '', // use sessin id if inbuilt pairing not working
   MODE: "public", // public or private
